999c2b30a9755314cb0606290b5ed8bca5523920
Timo Tyrrell <timomitchel23@gmail.com>
2018-02-21 12:16:49 -0700

    got close to post method creating a play for user story 3

--- spec/api/v1/game_request_spec.rb
+++ spec/api/v1/game_request_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe 'calid game response with game id and user scores'do 
-    let(:game) {create(:game)}
-    let(:user) {create(:user)}
-    let(:user_2) {create(:user)}
-    let(:play) {create(:play, game_id: game.id, user_id: user.id)}
-    let(:play_2) {create(:play, game_id: game.id, user_id: user_2.id)}
+    let!(:game) {create(:game)}
+    let!(:user) {create(:user)}
+    let!(:user_2) {create(:user)}
+    let!(:play) {create(:play, game_id: game.id, user_id: user.id)}
+    let!(:play_2) {create(:play, game_id: game.id, user_id: user_2.id)}
     it 'gets a specific json response when it runs a get' do
     
       get "/api/v1/games/#{game.id}"
@@ -19,7 +19,7 @@ describe 'calid game response with game id and user scores'do
   end
 
   it 'posts a play to the game' do
-    post "/api/v1/games/#{game.id}/plays"
+    post "/api/v1/games/#{game.id}/plays?user_id=#{user.id}&word=at"
 
     # game_response = JSON.parse(response.body)
 
